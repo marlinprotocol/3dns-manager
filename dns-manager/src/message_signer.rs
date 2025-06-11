772f23e6baa06612cd284b6e2542ac44d707dfc4
sagarparker <sagar8parker@gmail.com>
2025-06-11 11:53:46 +0530

    backup for the sign_typed_data call

--- dns-manager/src/message_signer.rs
+++ dns-manager/src/message_signer.rs
@@ -1,13 +1,26 @@
 use anyhow::{Context, Result};
-use alloy::{primitives::keccak256, signers::{k256::sha2::{Digest,Sha256}, local::PrivateKeySigner, SignerSync}};
+use alloy::{
+    primitives::{keccak256, B256},
+    sol,
+    signers::{local::PrivateKeySigner, SignerSync},
+};
+
+use alloy::sol_types::eip712_domain;
 
 use crate::namehash;
 
-pub struct MessageSigner{
+#[derive(Debug)]
+pub struct MessageSigner {
     key: Option<[u8; 32]>,
 }
 
-const set_record_typehash: &str = "setDNSRecords(bytes32 domain_id, bytes32 recordsHash)";
+sol! {
+    #[derive(Debug)]
+    struct SetRecordsParams {
+        bytes32 domain_id;
+        bytes32 records_hash;
+    }
+}
 
 impl MessageSigner {
     pub fn new() -> Self {
@@ -33,33 +46,29 @@ impl MessageSigner {
 
     pub async fn sign_message(&self, message: &str, domain: &str) -> Result<String> {
         let key = self.key.as_ref().context("Signer not initialized")?;
-
         let signer = PrivateKeySigner::from_bytes(key.into())?;
 
         let domain_id = namehash(domain);
-
-        let record_type_hash = keccak256(set_record_typehash.as_bytes());
-
         let hashed_records = keccak256(message.as_bytes());
 
-        let mut hasher = Sha256::new();
-        hasher.update(record_type_hash);
-        hasher.update(domain_id);
-        hasher.update(hashed_records);
-        let digest: [u8; 32] = hasher.finalize().into();
-
-        let signature = match signer.sign_hash_sync(&digest.into()) {
-            Ok(sig) => sig,
-            Err(e) => return Err(anyhow::anyhow!("Failed to sign message: {}", e)),
+        let params = SetRecordsParams {
+            domain_id: domain_id.into(),
+            records_hash: hashed_records.into(),
         };
+
+        // Create EIP-712 domain
+        let domain = eip712_domain! {
+            name: "3DNS Domain Manager",
+            version: "1",
+        };
+
+        let signature = signer.sign_typed_data_sync(&params, &domain)?;
 
         let signer_address = signer.address();
         let signer_creds = signer.credential();
         print!("Signer address: {}\n", signer_address);
         println!("Signer credentials: {:?}", signer_creds.verifying_key());
 
-        let signature_hex = hex::encode(signature.as_bytes());
-
-        Ok(signature_hex)
+        Ok(hex::encode(signature.as_bytes()))
     }
 }
